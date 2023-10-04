1989e6af387365022017a38686a04c6532186f60
John Koh <kohbokyan@gmail.com>
2023-10-04 13:58:01 +0800

    Add campaign button to profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,6 +2,7 @@
     <NavBar />
     <div class="container">
         <p>add something here</p>
+        <a class="btn btn-outline-success me-2" href="/campaign">Create Campaign</a>
     </div>
 </template>
 
